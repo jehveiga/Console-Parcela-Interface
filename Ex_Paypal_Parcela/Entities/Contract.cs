e2df7fba965e3423a08bee9c2b4f5f8678acf092
Jefferson Veiga <gelsvsampa@hotmail.com>
2022-02-24 08:51:54 -0300

    Testando o projeto passando os dados para calculo

--- Ex_Paypal_Parcela/Entities/Contract.cs
+++ Ex_Paypal_Parcela/Entities/Contract.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ex_Paypal_Parcela.Entities
+﻿namespace Ex_Paypal_Parcela.Entities
 {
     internal class Contract
     {
--- Ex_Paypal_Parcela/Program.cs
+++ Ex_Paypal_Parcela/Program.cs
@@ -1,9 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using Ex_Paypal_Parcela.Entities;
+using Ex_Paypal_Parcela.Services;
+using System.Diagnostics.Contracts;
+using System.Globalization;
+using Contract = Ex_Paypal_Parcela.Entities.Contract;
+
 Console.WriteLine("Enter contract data");
 Console.Write("Number: ");
-int number = int.Parse(Console.ReadLine());
+int contractNumber = int.Parse(Console.ReadLine());
 Console.Write("Date (dd/MM/yyyy): ");
-DateTime date = DateTime.Parse(Console.ReadLine());
-Console.Write("Contract value: " );
-double value = double.Parse(Console.ReadLine());
+DateTime contractDate = DateTime.Parse(Console.ReadLine());
+Console.Write("Contract value: ");
+double ContractValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Enter number of installments: ");
+int months = int.Parse(Console.ReadLine());
 
+Contract myContract = new Contract(contractNumber, contractDate, ContractValue);
+
+ContractService contractService = new ContractService(new PayPalService());
+contractService.ProcessContract(myContract, months);
+
+Console.WriteLine("Intallments: ");
+foreach(Installment installment in myContract.Installments)
+{
+    Console.WriteLine(installment);
+}
