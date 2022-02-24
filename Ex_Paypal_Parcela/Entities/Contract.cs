867eb9ae98931bd7b566fef8c59e3883ebacc412
Jefferson Veiga <gelsvsampa@hotmail.com>
2022-02-24 06:43:14 -0300

    Implementacao das entidades e comeco de configuracao de interacaoo com usuario

--- Ex_Paypal_Parcela/Entities/Contract.cs
+++ Ex_Paypal_Parcela/Entities/Contract.cs
@@ -11,6 +11,7 @@ namespace Ex_Paypal_Parcela.Entities
         public int Number { get; set; }
         public DateTime Date { get; set; }
         public double TotalValue { get; set; }
+        public Installment Installment { get; set; }
 
         public Contract(int number, DateTime date, double totalValue)
         {
--- Ex_Paypal_Parcela/Program.cs
+++ Ex_Paypal_Parcela/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter contract data");
+Console.Write("Number: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write("Date (dd/MM/yyyy): ");
+DateTime date = DateTime.Parse(Console.ReadLine());
+Console.Write("Contract value: " );
+double value = double.Parse(Console.ReadLine());
 
